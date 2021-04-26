a64763d1609af5f1a1b6b1ac0ee52910f23952e1
mdabdullahibnaharun <abdullah13426.cse@gmail.com>
2021-04-26 09:02:09 +0600

    Update Binary Search.cpp

--- Algorithm in Cpp/Binary Search.cpp
+++ Algorithm in Cpp/Binary Search.cpp
@@ -79,10 +79,11 @@ int main()
 
 // Binary Search in C++
 
+include <iostream>
+using namespace std;
 
 int binarySearch(int array[], int x, int low, int high) {
   
-	// Repeat until the pointers low and high meet each other
   while (low <= high) {
     int mid = low + (high - low) / 2;
 
@@ -100,7 +101,7 @@ int binarySearch(int array[], int x, int low, int high) {
 }
 
 int main(void) {
-  int array[] = {3, 4, 5, 6, 7, 8, 9};
+  int array[] = {3, 4, 5, 6, 7, 8};
   int x = 4;
   int n = sizeof(array) / sizeof(array[0]);
   int result = binarySearch(array, x, 0, n - 1);
